6484d54783a42936763f7ae7464ac7d698ef2e18
Jatish Khanna <jatish.khanna@gmail.com>
2020-02-11 12:16:52 +0530

    Create ErrorResponse.java

---
+++ src/main/java/com/example/startegysort/sorting/model/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.startegysort.sorting.model;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author Jatish_Khanna
+ *
+ */
+
+@Data
+public class ErrorResponse {
+  private Integer status;
+}
